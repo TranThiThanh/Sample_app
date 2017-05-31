507463122132bae8ebd574bcc5c180cbb51f13d4
TranThiThanh <tranthithanhqt1397@gmail.com>
2017-05-31 15:14:43 +0700

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,8 @@ ENV['RAILS_ENV'] ||= 'test'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  include ApplicationHelper
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
   # Add more helper methods to be used by all tests here...
 end
