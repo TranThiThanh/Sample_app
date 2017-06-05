ad0a0534c42971f9d3a646f0e872716697771a58
TranThiThanh <tranthithanhqt1397@gmail.com>
2017-06-05 09:37:41 +0700

    Finish user edit, update, index and destroy actions

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class ApplicationHelperTest < ActionView::TestCase
 	
   test "full title helper" do
-    assert_equal full_title,         FILL_IN
-    assert_equal full_title("Help"), FILL_IN
+    assert_equal full_title, "Ruby On Rails App"
+    assert_equal full_title("Help"), "Help | Ruby On Rails App"
   end
 end
