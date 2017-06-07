49c1dbe932526e023b8b5fba154e5be71e1745d2
TranThiThanh <tranthithanhqt1397@gmail.com>
2017-06-07 11:40:05 +0700

    Config send email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token, :reset_token
+  attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
   before_create :create_activation_digest
   	validates :name,  presence: true, length: { maximum: 50 }
@@ -57,13 +57,13 @@ class User < ApplicationRecord
   end
   private
 
-  def downcase_email
-      self.email = email.downcase
-  end
+    def downcase_email
+        self.email = email.downcase
+    end
 
-  def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-  end
+    def create_activation_digest
+        self.activation_token  = User.new_token
+        self.activation_digest = User.digest(activation_token)
+    end
   
 end
