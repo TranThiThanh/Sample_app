18fb8a7bc3d1a9a74b74115073b5a7c21cc7045f
TranThiThanh <tranthithanhqt1397@gmail.com>
2017-06-09 20:16:12 +0700

    Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,19 +57,19 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.default :charset => "utf-8" 
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true 
   host = 'arcane-savannah-43691.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.gmail.com',
-    :port           => '587',
+    :address        => 'mail.domain.com',
+    :port           => '25',
     :authentication => :plain,
     :user_name      => ENV['tranthithanh1397@gmail.com'],
     :password       => ENV['10102010'],
     :domain         => 'arcane-savannah-43691.herokuapp.com',
-    :enable_starttls_auto => true
+    :enable_starttls_auto => false
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
