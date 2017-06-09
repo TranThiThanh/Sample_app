2e625008d45d0b7f80c0ac2a66bfaf9472a188ce
TranThiThanh <tranthithanhqt1397@gmail.com>
2017-06-09 20:33:45 +0700

    Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'mail.domain.com',
-    :port           => '25',
+    :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['tranthithanh1397@gmail.com'],
     :password       => ENV['10102010'],
