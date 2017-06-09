2369081da47d1f2031e21f55ace9add2a5b1814b
TranThiThanh <tranthithanhqt1397@gmail.com>
2017-06-09 15:42:33 +0700

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,15 +43,15 @@ Rails.application.configure do
   # }
 
   config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'arcane-savannah-43691.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.perform_caching = false
   config.action_mailer.smtp_settings = {
-    address:              "smtp.sendgrid.net",
+    address:              "smtp.gmail.com",
     port:                 587,
-    domain:               'arcane-savannah-43691.herokuapp.com',
+    domain:               'gmail.com',
     user_name:            'tranthithanhqt1397@gmail.com',
     password:             '10102010',
     authentication:       :plain,
