897bf5a4c996d2795ef5e3b0b67f8110d00963c7
TranThiThanh <tranthithanhqt1397@gmail.com>
2017-06-09 15:20:34 +0700

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,14 +41,15 @@ Rails.application.configure do
   #   authentication:       :plain,
   #   enable_starttls_auto: true
   # }
+
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'arcane-savannah-43691.herokuapp.com'}
+  config.action_mailer.default_url_options = { host: 'arcane-savannah-43691.herokuapp.com' }
   config.action_mailer.perform_caching = false
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              "smtp.gmail.com",
     port:                 587,
     domain:               'arcane-savannah-43691.herokuapp.com',
     user_name:            'tranthithanhqt1397@gmail.com',
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,11 +69,11 @@ Rails.application.configure do
   #   :domain         => 'gmail.com',
   #   :enable_starttls_auto => true
   # }
+  config.action_mailer.default :charset => "utf-8" 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'arcane-savannah-43691.herokuapp.com'
   config.action_mailer.perform_deliveries = true  
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { :host => 'arcane-savannah-43691.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
     :port           => '587',
