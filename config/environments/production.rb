047a932a7a075f7eb675e60e0ab2b5afc367fbf2
TranThiThanh <tranthithanhqt1397@gmail.com>
2017-06-09 21:29:01 +0700

    Update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,8 +60,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true 
-  host = 'arcane-savannah-43691.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: 'arcane-savannah-43691.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
     :port           => '587',
