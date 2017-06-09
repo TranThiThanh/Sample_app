f6b6736bc3a531502ce7293391ac042d6fa6d52c
TranThiThanh <tranthithanhqt1397@gmail.com>
2017-06-09 15:09:28 +0700

    update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,8 +45,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  host = 'arcane-savannah-43691.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  config.action_mailer.default_url_options = { host: 'arcane-savannah-43691.herokuapp.com'}
   config.action_mailer.perform_caching = false
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
